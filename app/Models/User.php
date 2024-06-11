a3de0be885918bd90d8ccdb61a207b280fb21676
kuhakuni <afif.haidar@gmail.com>
2024-06-11 13:54:41 +0700

    refactor: Update user creation in UserFactory
    The UserFactory file was updated to include a 'username' field along with the 'email' field in the default state definition. This change ensures that the 'username' field is populated when creating a new user using the factory.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,27 +2,27 @@
 
 namespace App\Models;
 
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
-    protected $fillable = [
-        'email',
-        'username',
-        'password',
-        'role',
-    ];
+class User extends Authenticatable {
+  use HasFactory;
+  protected $fillable = [
+    'email',
+    'username',
+    'password',
+    'role',
+  ];
 
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
 
-    public $timestamps = false;
+  public $timestamps = false;
 
-    // public function setPasswordAttribute($value)
-    // {
-    //     $this->attributes['password'] = bcrypt($value);
-    // }
+  // public function setPasswordAttribute($value)
+  // {
+  //     $this->attributes['password'] = bcrypt($value);
+  // }
 }
