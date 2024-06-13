2252664fdef10f8e45ec9e3991f39cad6ff47876
ferdihassan20 <154717978+ferdihassan20@users.noreply.github.com>
2024-06-13 17:02:04 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,42 +6,51 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 //HOME
-Route::get('/home', [HomeController::class, 'index']);
-Route::post('/home', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 //COMMUNITY
 Route::get('/community', function () {
-    return view('community');
-})->name('community');
+  return view('community');
+})->name('community')->middleware('auth');
 
 //DASHBOARD
 Route::get('/dashboard', function () {
-    return view('dashboard');
+  return view('dashboard');
 })->name('dashboard');
 
 //LOGIN
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.auth');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 
 //LOGOUT
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
 //REGISTRASI
 Route::get('/register', function () {
-    return view('register');
-})->name('register');
+  return view('register');
+})->name('register')->middleware('guest');
 
-Route::post('/register', [AuthController::class, 'store'])->name('register.store');
+Route::post('/register', [AuthController::class, 'register'])->name('register')->middleware('guest');
 
 //HOME
 Route::get('/', function () {
-    return view('welcome');
+  return view('dashboard');
 });
 
 //ADMIN
-Route::get('/admin', [UserController::class, 'index'])->name('admin');
+Route::get('/admin', [UserController::class, 'index'])->name('admin')->middleware('auth');
+// Route to display the edit form for a specific user
+Route::get('/edit/{id}', [UserController::class, 'edit'])->name('edit')->middleware('auth');
+
+Route::post('/update', [UserController::class, 'update'])->name('edit')->middleware('auth');
+
 
 // User CRUD routes
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+Route::post('/add', [UserController::class, 'add'])->name('add')->middleware('auth');
+Route::put('/users/{id}', [UserController::class, 'update'])->name('update')->middleware('auth');
+Route::delete('/destroy', [UserController::class, 'destroy'])->name('destroy')->middleware('auth');
+
+// Route::post('/users', [UserController::class, 'register'])->name('users.register')->middleware('auth');
+// Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update')->middleware('auth');
+// Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');
